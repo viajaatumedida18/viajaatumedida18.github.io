18b6d38c0fe798c9c8f81419ea05ddfb775cf7d6
Gabriel López <gabolopez1990@gmail.com>
2018-07-16 15:53:06 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
-var tasaToday = 248;
-var pesos = 0;
+var imgCant = 20;
+var imgUsed = [];
 
-$("#dia").html(tasaToday);
+for (let i=0; i <= 9; i++){
+	var random = randomNum();
+	imgUsed.push(random);
+	$("#prueba").append('<img id="img'+i+'" src="img/venezuela'+random+'.jpg">');
+};
 
-$("#userPesos").click(function(){
-  pesos = $("#cambio").val();
-  if($.isNumeric(pesos)){
-    var total = pesos * tasaToday;
-    $("#total").html("A la tasa de hoy son <strong>"+ total+ "</strong>" + " Bolívares");
-  }
-  else {
-    $("#total").html("Ingrese un número para poder calcular y use '.' para los decimales.");
-  }
-});
+
+
+function randomNum() {
+    var rand = Math.floor(Math.random() * imgCant + 1);
+    if($.inArray(rand,imgUsed) !== -1){
+    	return randomNum();    	
+	}
+	else {		
+		return rand;
+	}
+}
+
+function changeImg(){
+	var randImg = randomNum();
+	var change = Math.floor(Math.random() * 9 + 1);
+	imgUsed[change] = randImg;
+	$('#img'+change).fadeOut("fast").attr("src","img/venezuela"+randImg+".jpg").fadeIn("fast");
+	console.log(imgUsed);
+	return randImg;
+}
+
+window.setInterval(changeImg,5000);
